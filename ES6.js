d7683df2f684a49ee59957aadfee71fc2428efd8
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-02-22 14:08:30 +0600

    default value added

--- ES6.js
+++ ES6.js
@@ -6,3 +6,6 @@ const text = `I am a web developer.
 I love to code.
 I love to eat biryani.`;
 console.log(text);
+
+const defaultValue = (num1, num2 = 0) => num1 + num2;
+console.log(defaultValue(10));
