64f79a8ef536153e79ed3ec77ed9253faf5a033e
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-02-22 14:06:19 +0600

    secent logic added

--- ES6.js
+++ ES6.js
@@ -1,3 +1,8 @@
 const multiple = (num1, num2, num3) => num1 * num2 * num3;
 
 console.log(multiple(3,4,6));
+
+const text = `I am a web developer.
+I love to code.
+I love to eat biryani.`;
+console.log(text);
