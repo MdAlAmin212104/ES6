67174ece101b2557464c559c2de090cfc1dfa29e
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-02-22 14:19:54 +0600

    problem 2 num1 solved

--- ES6.js
+++ ES6.js
@@ -1,11 +1,28 @@
 const multiple = (num1, num2, num3) => num1 * num2 * num3;
 
-console.log(multiple(3,4,6));
+//console.log(multiple(3,4,6));
 
 const text = `I am a web developer.
 I love to code.
 I love to eat biryani.`;
-console.log(text);
+//console.log(text);
 
 const defaultValue = (num1, num2 = 0) => num1 + num2;
-console.log(defaultValue(10));
+//console.log(defaultValue(10));
+
+
+// Problem 2 solved
+
+const friend = ['Rahim', 'Karim', 'Ali', 'Jaba', 'Salam', 'Abul', 'Jabu'];
+
+const eventFriend = (friends) => {
+      let newArray = [];
+      for (const friend of friends) {
+            if(friend.length % 2 === 0){
+                  newArray.push(friend);
+                  
+            }
+      }
+      return newArray;
+}
+//console.log(eventFriend(friend));
