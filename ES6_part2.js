c4cd631a00cae81b5df4e3df4656316afee71568
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-02-23 20:53:57 +0600

    task 2 solved

--- ES6_part2.js
+++ ES6_part2.js
@@ -20,4 +20,33 @@ let data ={
       }
 }
 
-console.log(data.shopia.study[1].secondary[1].location);
+//console.log(data.shopia.study[1].secondary[1].location);
+
+
+//task 2 : console log petersburg and Herry
+let students = {
+      2222 :{
+            name : "jack",
+            section : "C",
+            class : "IX",
+            address : {
+                  "building no" : 12,
+                  "street" : "St Jonson",
+                  "city" : "petersburg",
+                  "country" : "UK"
+            }
+      },
+      3333 : {
+            name : "Herry",
+            section : "D",
+            class : "x",
+            address : {
+                  "building no" : 85,
+                  "street" : "DC Road",
+                  "city" : "kakurhat",
+                  "country" : "bangladesh"
+            }
+      }
+}
+console.log(students[2222].address['city']);
+console.log(students[3333].name);
